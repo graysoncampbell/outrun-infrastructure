f05b43b58c8e7cb32037b46ab94e555364bd5d79
Grayson Campbell <grayson.campbell@papercut.com>
2023-10-25 22:02:10 +1100

    Feat: Configured onepassword secrets, bun scripts  & default mongodb

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "mongodb_atlas_api_pub_key" {
+  default = "op://dev/mongodb_atlas/public_api_key"
+}
+
+variable "mongodb_atlas_api_pri_key" {
+  default = "op://dev/mongodb_atlas/private_api_key"
+}
+
+variable "mongodb_atlas_org_id" {
+  default = "op://dev/mongodb_atlas/org_id"
+}
+
+variable "mongodb_atlas_api_pri_key" {
+  default = "op://dev/mongodb_atlas/private_api_key"
+}
+
+variable "mongodb_atlas_database_username" {
+  default = "op://dev/mongodb_atlas/username"
+}
+
+variable "mongodb_atlas_database_user_password" {
+  default = "op://dev/mongodb_atlas/username"
+}
+
+variable "mongodb_whitelist" {
+  default = "op://dev/mongodb_atlas/whitelist_ip"
+}
