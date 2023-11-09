2b59aa571b2a081b1f6845d71806d0ca1dffbf10
Grayson Campbell <grayson.campbell@papercut.com>
2023-11-09 23:28:03 +1100

    Feat: My first function

--- mongo/functions/myFunction.js
+++ mongo/functions/myFunction.js
@@ -1,12 +1,3 @@
-exports = async function hello(...args) {
-    // Write your function logic here! You can...
-    // Import dependencies
-    const assert = require("assert")
-    assert(typeof args[0] === "string")
-    // Use ES6+ syntax
-    const sayHello = (name = "world") => {
-      console.log(`Hello, ${name}.`)
-    }
-    // Return values back to clients or other functions
-    return sayHello(args[0])
-  }
+exports = function(name) {
+  return `Hello, ${name ?? "stranger"}!`
+}
